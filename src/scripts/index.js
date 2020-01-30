9ec900b19209bb49279586c9def8bcb86bb395a3
Pollak, Baron <BaronPollak@quickenloans.com>
2020-01-30 11:55:59 -0500

    adding in some table functionality and new calculation
    adding in revealing the table once you put in an amount, and adjust the totaltable function to add in income

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -21,10 +21,12 @@ function insertItem() {
      * Below is the javascript to create the entry into the table at the bottom of the page.
      */
 
-
     if(costName !== '' && costAmount !== '' && dateOfCost !== ''){
 
         createTableEntry(tableName, 'cost_', costName, costAmount, dateOfCost);
+
+
+        //TODO check to see if hide class exists on table div if it does do nothing, if it doesn't add it
 
         //These are here to zero out the fields after valid submission
         document.getElementById('item_name_field').value = '';
@@ -55,6 +57,8 @@ function insertItem() {
 
 deleteRow = (tableName) => {
 
+    //TODO if you delete all entries and there is none left, you ened to hide table
+
     $(tableName).on('click', '.fa-window-close', function() {
         $(this).closest("tr").remove();
     });
@@ -70,6 +74,10 @@ refreshTotal = () => {
 
 
 createTableEntry = (tableName, className, itemName, amountOfItem, dateOfItem ) => {
+
+    let incomeTableClass = document.getElementById("income-table-cell");
+    let costTableClass = document.getElementById("cost-table-cell");
+    let tableContainer = document.getElementById("table-container");
 
     document.getElementById(tableName).getElementsByTagName('tbody')[0].insertRow(0).innerHTML =
         '<tr>' +
@@ -95,34 +103,85 @@ createTableEntry = (tableName, className, itemName, amountOfItem, dateOfItem ) =
         '</td>' +
         '</tr>';
 
+    if(tableContainer.classList.contains("hide")){
+
+        tableContainer.classList.remove("hide");
+        console.log("The hole container is hidden");
+
+    } if(incomeTableClass.classList.contains("hide")  && tableName === 'income_table'){
+
+        incomeTableClass.classList.remove("hide");
+        console.log("I need to unhide Income Table");
+
+    } if (costTableClass.classList.contains("hide") && tableName === 'cost_table') {
+
+        costTableClass.classList.remove("hide");
+        console.log("I need to unhide Cost Table");
+
+    }
+
 };
 
 tableTotal = (tableName) => {
 
     let totalLeftOver = document.getElementById('totalLeftOver').innerHTML;
-    totalLeftOver = totalLeftOver.replace(/[^0-9.-]+/g, '');
+    totalLeftOver = parseFloat(totalLeftOver.replace(/[^0-9.-]+/g, ''));
 
-    let tableBody = document.getElementById('cost_table');
+    let tableBody = document.getElementById(tableName);
     let whichColumn = 1; // which column in the table has what we want
     let howManyRows = tableBody.rows.length;
     let totalCostAmount = 0;
+    let totalIncome = 0;
 
-    for (let i=1; i<(howManyRows-1); i++) // skip first and last row (hence i=1, and howManyRows-1)
-    {
-        let thisTrElem = tableBody.rows[i];
-        let thisTdElem = thisTrElem.cells[whichColumn];
-        let thisTextNode = thisTdElem.childNodes.item(0);
+    tableName = tableName.toString();
 
-        thisTextNode = thisTextNode.data.replace(/[^0-9.-]+/g, '');
-        let actualNumber = parseFloat(thisTextNode);
-        // if you didn't get back the value NaN (i.e. not a number), add into result
-        if (!isNaN(actualNumber))
-            totalCostAmount += actualNumber;
+    if(tableName === 'cost_table') {
+
+        for (let i=1; i<(howManyRows-1); i++) // skip first and last row (hence i=1, and howManyRows-1)
+        {
+            let thisTrElem = tableBody.rows[i];
+            let thisTdElem = thisTrElem.cells[whichColumn];
+            let thisTextNode = thisTdElem.childNodes.item(0);
+
+            thisTextNode = thisTextNode.data.replace(/[^0-9.-]+/g, '');
+            let actualNumber = parseFloat(thisTextNode);
+            // if you didn't get back the value NaN (i.e. not a number), add into result
+            if (!isNaN(actualNumber))
+                totalCostAmount += actualNumber;
+        }
+
+        console.log(totalLeftOver);
+
+        let numberTotal = totalLeftOver - totalCostAmount;
+
+        return document.getElementById('totalLeftOver').innerHTML = '$' + numberTotal;
+
+    } else if ( tableName === 'income_table' ){
+
+        for (let i=1; i<(howManyRows-1); i++)
+        {
+
+            let thisTrElem = tableBody.rows[i];
+            let thisTdElem = thisTrElem.cells[whichColumn];
+            let thisTextNode = thisTdElem.childNodes.item(0);
+
+            thisTextNode = thisTextNode.data.replace(/[^0-9.-]+/g, '');
+            let actualNumber = parseFloat(thisTextNode);
+            console.log('This is before adding',actualNumber);
+            // if you didn't get back the value NaN (i.e. not a number), add into result
+            if (!isNaN(actualNumber))
+              totalIncome += actualNumber;
+        }
+
+        let numberTotal = totalLeftOver + totalIncome;
+
+        console.log('This is after total', numberTotal);
+
+        return document.getElementById('totalLeftOver').innerHTML = '$' + numberTotal;
+
     }
 
-    let numberTotal = totalLeftOver - totalCostAmount;
 
-    return document.getElementById('totalLeftOver').innerHTML = '$' + numberTotal;
 
 
 };
